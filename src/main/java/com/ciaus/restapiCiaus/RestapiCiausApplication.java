6a5755a3e3fda6aea0b637112398bdfb37bd768a
xtremeboombox1 <xtremeboombox1@gmail.com>
2025-01-19 15:57:47 -0300

    first

---
+++ src/main/java/com/ciaus/restapiCiaus/RestapiCiausApplication.java
@@ -0,0 +1,14 @@
+package com.ciaus.restapiCiaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiCiausApplication {
+
+	// test
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiCiausApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciaus/restapiCiaus/RestapiCiausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciaus.restapiCiaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiCiausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
